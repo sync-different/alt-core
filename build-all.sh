b1be6816ccee08a5984d55657df1688a6788a9e5
agf2013 <agoyen@gmail.com>
2025-06-25 17:05:45 -0700

    Fix build script to install JAR in local Maven

--- build-all.sh
+++ build-all.sh
@@ -1,22 +1,30 @@
 #cass-server.jar
-cd cass-server-maven
-mvn clean
-mvn compile
-mvn package
-cd ..
-cp cass-server-maven/target/cass-server-1.0.0.jar ./rtserver-maven/lib/cass-server.jar
+#cd cass-server-maven
+#mvn clean
+#mvn compile
+#mvn package
+#cd ..
+#cp cass-server-maven/target/cass-server-1.0.0.jar ./rtserver-maven/lib/cass-server.jar
 
 #rtserver.jar
-cd rtserver-maven
+#cd rtserver-maven
+#mvn clean
+#mvn compile
+#mvn package
+#cd ..
+#cp rtserver-maven/target/rtserver-1.0-SNAPSHOT.jar ./scrubber/repo/rtserver.jar
+
+#scrubber.jar
+cd scrubber-maven
 mvn clean
 mvn compile
 mvn package
 cd ..
-cp rtserver-maven/target/rtserver-1.0-SNAPSHOT.jar ./scrubber/repo/rtserver.jar
+cp scrubber-maven/target/scrubber.jar ./scrubber/repo/scrubber.jar
 
 #uber jar
-cd scrubber
-mvn clean
-mvn compile
-mvn package
-
+#cd scrubber
+#./install.sh
+#mvn clean
+#mvn compile
+#mvn package
