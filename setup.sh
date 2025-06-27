a5e13bf517afef349bb78a54b5da29149134c7e9
agf2013 <agoyen@gmail.com>
2025-06-26 19:42:53 -0700

    add script to setup folders for linux

---
+++ setup.sh
@@ -0,0 +1,16 @@
+mkdir scrubber/incoming
+mkdir scrubber/data
+mkdir scrubber/data/localdb
+mkdir scrubber/data/localdb/BackupJobs
+mkdir scrubber/data/localdb/BatchJobs
+mkdir scrubber/data/localdb/NodeInfo
+mkdir scrubber/data/localdb/Standard1
+mkdir scrubber/data/localdb/Standard2
+mkdir scrubber/data/localdb/Super2/
+mkdir scrubber/data/localdb/Super2/hashes
+mkdir scrubber/data/localdb/Super2/hashesm
+mkdir scrubber/data/localdb/Super2/paths
+mkdir scrubber/outgoing
+mkdir scrubber/log
+mkdir web/cass/pic
+
