a6e994ae2ba4bc53d89993b70f15aab8cad3d8cd
agf2013 <agoyen@gmail.com>
2025-07-01 13:59:03 -0700

    Update build-clean.sh

--- build-clean.sh
+++ build-clean.sh
@@ -1,8 +1,9 @@
+echo "cass-server-maven"
+rm -r cass-server-maven/target/*
 echo "scrubber"
 rm -r scrubber/target/*
 echo "scrubber-maven"
 rm -r scrubber-maven/target/*
 echo "rtserver-maven"
 rm -r rtserver-maven/target/*
-
 
