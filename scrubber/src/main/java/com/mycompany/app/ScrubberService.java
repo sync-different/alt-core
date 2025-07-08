affd22d7f89d7e463e7dc30d9d0fc0da9fce8aed
agf2013 <agoyen@gmail.com>
2025-07-08 15:13:47 -0700

    Control threads via Arg parms
    uber app contains 2 params
    arg[0] = 0/1 web server
    arg[1] = flags to control what scrubber threads to launch (c s t, etc..)
    useful for debugging

--- scrubber/src/main/java/com/mycompany/app/ScrubberService.java
+++ scrubber/src/main/java/com/mycompany/app/ScrubberService.java
@@ -5,8 +5,15 @@ import main.Main;
 public class ScrubberService implements Runnable {
         
         private Boolean mTerminated = false;
+        private String[] arguments;
+        private String params = "";
 
-        public ScrubberService() {
+        public ScrubberService(String args[]) {
+            arguments = args;
+            if (args != null) {
+                System.out.println("args len = " + args.length);
+                if (args.length > 1) params = args[1]; 
+            }
             Thread t;
             t = new Thread(this,"scrubber");
             t.start();
@@ -15,8 +22,13 @@ public class ScrubberService implements Runnable {
         public void run () {
             while (!mTerminated) {
                 Main mymain = new Main();
-                String[] myargs = new String[]{"data/records.db","localhost","8081","/Users/","../rtserver/incoming","macprd","config/www-rtbackup.properties"};
-
+                System.out.println("scrubber launch flags = " + params);
+                String[] myargs;
+                if (params.length() > 0) {
+                    myargs = new String[]{"data/records.db","localhost","8081","/Users/","../rtserver/incoming","macprd","config/www-rtbackup.properties",params};
+                } else {
+                    myargs = new String[]{"data/records.db","localhost","8081","/Users/","../rtserver/incoming","macprd","config/www-rtbackup.properties"};                    
+                }
                 try {
                     mymain.main(myargs);
                 } catch (Exception e) {
