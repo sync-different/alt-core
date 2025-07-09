ed241c5eafc53b18b468d51a970a0146c52b6eb4
agf2013 <agoyen@gmail.com>
2025-07-09 15:36:11 -0700

    explorations with ollama

---
+++ alt-common/src/main/java/com/alterante/ollama/MyTest.java
@@ -0,0 +1,11 @@
+package com.alterante.ollama;
+
+public class MyTest {
+     public static void main(String[] args) {
+
+            System.out.println("hello ollama2");
+            //OllamaChatModelTest tester = new OllamaChatModelTest();
+            OllamaStreamingChatModelTest tester2 = new OllamaStreamingChatModelTest();
+            System.out.println("bye...");
+     }
+    }
