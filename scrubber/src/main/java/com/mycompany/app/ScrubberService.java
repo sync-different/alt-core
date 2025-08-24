f585b617ccac67421f9dc417a3b4446cd101a192
agf2013 <agoyen@gmail.com>
2025-08-24 10:47:24 -0700

    [fix] hardcoded port 8081
    args were passing hardcoded port 8081.  instead, fetch port from rtserver/config/www-server.properties

--- scrubber/src/main/java/com/mycompany/app/ScrubberService.java
+++ scrubber/src/main/java/com/mycompany/app/ScrubberService.java
@@ -1,5 +1,12 @@
 package com.mycompany.app;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import main.Main;
 
 public class ScrubberService implements Runnable {
@@ -7,6 +14,10 @@ public class ScrubberService implements Runnable {
         private Boolean mTerminated = false;
         private String[] arguments;
         private String params = "";
+        protected static Properties props = new Properties();
+
+
+        String mLocalPort = "8081";
 
         public ScrubberService(String args[]) {
             arguments = args;
@@ -20,14 +31,19 @@ public class ScrubberService implements Runnable {
         }
 
         public void run () {
+            try {
+                loadProps();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             while (!mTerminated) {
                 Main mymain = new Main();
                 System.out.println("scrubber launch flags = " + params);
                 String[] myargs;
                 if (params.length() > 0) {
-                    myargs = new String[]{"data/records.db","localhost","8081","/Users/","../rtserver/incoming","macprd","config/www-rtbackup.properties",params};
+                    myargs = new String[]{"data/records.db","localhost",mLocalPort,"/Users/","../rtserver/incoming","macprd","config/www-rtbackup.properties",params};
                 } else {
-                    myargs = new String[]{"data/records.db","localhost","8081","/Users/","../rtserver/incoming","macprd","config/www-rtbackup.properties"};                    
+                    myargs = new String[]{"data/records.db","localhost",mLocalPort,"/Users/","../rtserver/incoming","macprd","config/www-rtbackup.properties"};                    
                 }
                 try {
                     mymain.main(myargs);
@@ -36,4 +52,36 @@ public class ScrubberService implements Runnable {
                 }
             }
         }
+
+        void loadProps() throws IOException {
+            try {
+                props = new Properties();
+                System.out.println("loadProps()");
+                File f = new File
+                        (
+                        ".."+
+                        File.separator+
+                        "rtserver"+
+                        File.separator+
+                        "config"+
+                        File.separator+
+                        "www-server.properties");
+                if (f.exists()) {
+                        InputStream is =new BufferedInputStream(new
+                                    FileInputStream(f));
+                        props.load(is);
+                        is.close();
+                        String r = props.getProperty("port");
+                        if (r != null) {
+                            mLocalPort = r;
+                        } else {
+                           System.out.println("**** ScrubberService: port not found. Using default."); 
+                        }
+                        System.out.println("**** ScrubberService: loaded port "+mLocalPort);
+                    }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
     }
