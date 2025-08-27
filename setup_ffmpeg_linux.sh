6f7916c74adcd551dc86a880a94ad0126809c026
agf2013 <agoyen@gmail.com>
2025-08-27 12:29:49 -0700

    Update setup script linux
    removed the -j flag

--- setup_ffmpeg_linux.sh
+++ setup_ffmpeg_linux.sh
@@ -1,5 +1,4 @@
 curl -L https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz -o ffmpeg.tar.xz
-tar -xjvf ffmpeg.tar.xz
+tar -xvf ffmpeg.tar.xz
 cp ./ffmpeg-master-latest-linux64-gpl/bin/ffmpeg ../
 cp ./ffmpeg.txt ../
-
