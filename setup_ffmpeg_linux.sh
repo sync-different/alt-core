2f5fb62139cafc6905de6ef845792cbbc4adc237
agf2013 <agoyen@gmail.com>
2025-08-27 12:18:29 -0700

    add scripts for setup - video streaming

---
+++ setup_ffmpeg_linux.sh
@@ -0,0 +1,5 @@
+curl -L https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz -o ffmpeg.tar.xz
+tar -xjvf ffmpeg.tar.xz
+cp ./ffmpeg-master-latest-linux64-gpl/bin/ffmpeg ../
+cp ./ffmpeg.txt ../
+
---
+++ setup_ffmpeg_mac.sh
@@ -0,0 +1,5 @@
+curl https://evermeet.cx/ffmpeg/ffmpeg-120820-g0f873fc07a.zip -o ffmpeg.zip
+unzip -o ffmpeg.zip
+cp ffmpeg ../
+cp ffmpeg.txt ../
+
