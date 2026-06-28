78267c789c3a880f28ffe1b8326fa4b4e1a1c7cc
Alejandro Goyen <agoyen@gmail.com>
2026-06-27 19:56:33 -0700

    Updated Cass7Funcs - fix compile
    Fix compilation libthrift

--- cass-server-maven/src/main/java/utils/Cass7Funcs.java
+++ cass-server-maven/src/main/java/utils/Cass7Funcs.java
@@ -38,7 +38,7 @@ import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
-import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.layered.TFramedTransport;
 
 
 import java.util.*;
